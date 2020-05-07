d3c5f81e69eccb39e11c5a680b48e93e2afcb81e
Pricilla Souza <pricilla_souzacs@hotmail.com>
2020-05-06 21:51:57 -0300

    Implementação do endpoint deletar postagem

--- blogPessoal/src/app/feed/feed.component.ts
+++ blogPessoal/src/app/feed/feed.component.ts
@@ -15,12 +15,27 @@ export class FeedComponent implements OnInit {
   listaPostagens: Postagem[]
   postagem: Postagem = new Postagem
 
+  alerta:boolean = false
 
   constructor(private postagemService: PostagemService) { }
 
   ngOnInit(): void {
     this.findallPostagens()
     window.scroll(0,0)
+
+    let item: string = localStorage.getItem('delOk')
+
+    if (item == "true"){
+      this.alerta = true
+      localStorage.clear()
+
+      setTimeout(()=>{
+        location.assign('/feed')
+
+      }, 3000);
+
+    }
+
   }
 
   findallPostagens() {
